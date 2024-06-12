0efdafa80dad41b398483b88e9d2a7a2152c7e38
shreenivask <shreenivask@users.noreply.github.com>
2024-06-12 13:26:08 +0530

    Update HelloWorld.php

---
+++ src/HelloWorld.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shreenivas\HelloWorld;
+
+final class HelloWorld
+{
+    public function hello(): string
+    {
+        return 'Hello World!';
+    }
+}
